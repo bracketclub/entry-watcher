d6b91a5f67d01cc8e066e6a07d7e7feef67d200d
Luke Karrys <lukekarrys@gmail.com>
2013-03-22 16:23:35 -0700

    watch twitter

--- app.js
+++ app.js
@@ -3,13 +3,18 @@
 /*global console */
 
 var express = require('express'),
+    appPackage = require('./package'),
+    config = require('./config'),
     http = require('http'),
     log = require('./lib/log'),
     app = express(),
-    twitter = require('./lib/twitter');
+    TwitterWatcher = require('./lib/twitter'),
+    twitter = new TwitterWatcher({appName: appPackage.name, tag: config.twitter.hashtags[0]}),
+
+    app = express();
 
 app.configure(function () {
-  app.set('port', 8080);
+  app.set('port', 8081);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
