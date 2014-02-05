b2ba5bcd0fe66de65be9f8965c54aa0095c91011
Luke Karrys <lukekarrys@gmail.com>
2014-02-04 23:45:57 -0700

    simplified single file

---
+++ index.js
@@ -0,0 +1,74 @@
+var HASHTAGS = ['tybrkt'],
+    DOMAIN = 'tweetyourbracket.com',
+    logger = require('bucker').createLogger({
+        console: {
+            color: true
+        }
+    }),
+    year = '2013',
+    config = require('./config.js'),
+    Twit = require('twit'),
+    BracketFinder = require('bracket-finder'),
+    finder = new BracketFinder({domain: DOMAIN, hashtags: HASHTAGS, year: year}),
+    Database = new require('db-schema'),
+    moment = require('moment'),
+    bracketsClose = moment(finder.locks),
+    now = moment();
+
+if (now.isBefore(bracketsClose)) {
+    logger.debug('[START STREAM]', 'track:' + HASHTAGS.toString(), 'until', bracketsClose.calendar(), '/', bracketsClose.fromNow());
+
+    var db = new Database(config.db);
+    var T = new Twit(config.twitter);
+    var stream = T.stream('statuses/filter', {track: HASHTAGS});
+    stream.on('tweet', function (data) {
+        if (!data.hasOwnProperty('retweeted_status')) {
+            var tweetLink = 'twitter.com/' + data.user.screen_name + '/status/' + data.id_str;
+
+            logger.debug('[TWEET]', tweetLink);
+            finder.find(data, function (err, res) {
+                if (err) return logger.warn('[TWEET]', err.message, tweetLink);
+
+                var validBracket = res,
+                    bracketTime = moment(data.created_at),
+                    record = {};
+
+                if (validBracket && bracketTime.isBefore(bracketsClose)) {
+                    record = {
+                        created: data.created_at,
+                        bracket: validBracket.toUpperCase(),
+                        user_id: data.user.id_str,
+                        tweet_id: data.id_str,
+                        username: data.user.screen_name,
+                        name: data.user.name,
+                        profile_pic: data.user.profile_image_url
+                    };
+
+                    db.upsertBracket(record, function (err, doc) {
+                        if (err) return logger.error('[SAVE ERROR]', tweetLink);
+                        logger.debug('[SAVE SUCESS]', tweetLink);
+                    });
+                }
+            });
+        }
+    });
+
+    stream.on('disconnect', function (message) {
+        logger.error('[DISCONNECT]', message);
+    });
+
+    stream.on('connect', function (request) {
+        logger.error('[CONNECT]');
+    });
+
+    stream.on('reconnect', function (request, response, connectInterval) {
+        logger.error('[RECONNECT]');
+    });
+
+    setTimeout(function () {
+        logger.debug('[STOP STREAM]', 'at', bracketsClose.calendar(), '/', bracketsClose.fromNow());
+        stream.stop();
+    }, bracketsClose.diff(now));
+} else {
+    return logger.info('[NO STREAM]', 'at', bracketsClose.calendar(), '/', bracketsClose.fromNow());
+}
